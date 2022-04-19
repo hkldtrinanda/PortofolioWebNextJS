806f8d87a25032d681b39d2fb0008612c0c8f52b
hkldtrinanda <77687182+hkldtrinanda@users.noreply.github.com>
2022-04-19 12:02:35 +0700

    up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,9 @@ import Head from "next/head";
 import MetaTags from "@/components/MetaTags";
 
 const metaData = {
-    title: "Portofolio Website Hkldtrinanda",
-    description: "Portofolio Website Hkldtrinanda",
-    url: "https://www.hkldtrinanda.works",
+    title: "Portofolio Website",
+    description: "With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!",
+    url: "https://www.example.com",
     image: "https://www.example.com/image.jpg",
     themeColor: "#ffffff",
     keywords: "key1, key2, key3, key4, key5, key6",
